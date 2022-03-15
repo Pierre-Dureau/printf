3fc21462e8debf9260a82dade322193eb778fcb9
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-15 13:55:56 +0100

    Fix flag

--- print_ptr.c
+++ print_ptr.c
@@ -11,23 +11,14 @@ int print_adresse(va_list list, flag_t flag)
 {
 	unsigned long int num = va_arg(list, unsigned long int);
 
+	(void)flag;
+
 	if (num)
 	{
 		static const char number[] = "0123456789abcdef";
 		static char buffer[20];
 		char *ptr;
 		int m = 0;
-
-		if (flag.plus == 1)
-		{
-			_putchar('+');
-			m = 1;
-		}
-		if (flag.space == 1 && flag.plus != 1)
-		{
-			_putchar(' ');
-			m = 1;
-		}
 
 		ptr = &buffer[19];
 		*ptr = '\0';
